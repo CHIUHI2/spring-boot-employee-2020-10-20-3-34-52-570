4bd3c3c16b7ed65c96975742da696beffc3cf833
Hins Chiu <chiuhi2@oocl.com>
2020-12-02 16:12:50 +0800

    [employee][HinsChiu]feat:add delete API for EmployeeController

--- src/main/java/com/thoughtworks/springbootemployee/Employee/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/Employee/EmployeeController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.Employee;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -69,5 +70,12 @@ public class EmployeeController {
 
             return ResponseEntity.ok(requestEmployee);
         }
+    }
+
+    @DeleteMapping("/{employeeId}")
+    public ResponseEntity<Void> delete(@PathVariable Integer employeeId) {
+        boolean isDeleted = this.employees.removeIf(employee -> employee.getId().equals(employeeId));
+
+        return isDeleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build() ;
     }
 }
