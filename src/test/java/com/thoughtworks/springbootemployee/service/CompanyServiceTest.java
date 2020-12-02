5617097b2b9a5251bde9717cce158870250e60b9
Hins Chiu <chiuhi2@oocl.com>
2020-12-02 22:19:44 +0800

    [company][HinsChiu]test:implement test for should return correct Employees when CompanyServie findCompanyEmployeesById given found Company id

--- src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.service;
 
 import com.thoughtworks.springbootemployee.dto.Company;
+import com.thoughtworks.springbootemployee.dto.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -72,5 +73,28 @@ public class CompanyServiceTest {
 
         //then
         assertNull(company);
+    }
+
+    @Test
+    void should_return_correct_employees_when_find_company_employees_by_id_given_found_id() {
+        //given
+        Company company1 = new Company(1, "Company1");
+
+        Employee employee1 = new Employee(1, "Sam", 20, "Male", 20000);
+        company1.addEmployee(employee1);
+
+        Employee employee2 = new Employee(2, "Ken", 20, "Male", 20000);
+        company1.addEmployee(employee2);
+
+        Company company2 = new Company(2, "Company2");
+
+        when(this.companyRepository.findAll()).thenReturn(Arrays.asList(company1, company2));
+        when(this.companyRepository.findCompanyById(1)).thenCallRealMethod();
+
+        //when
+        List<Employee> employees = this.companyService.findCompanyEmplyeesById(1);
+
+        //then
+        assertEquals(Arrays.asList(employee1, employee2), employees);
     }
 }
