4c758e2db8b6fa79d855ae58c28bade658b53120
Hins Chiu <chiuhi2@oocl.com>
2020-12-02 21:34:14 +0800

    [company][HinsChiu]test:init Test

---
+++ src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class CompanyServiceTest {
+    @Mock
+    CompanyRepository companyRepository;
+
+    @InjectMocks
+    CompanyService companyService;
+}
