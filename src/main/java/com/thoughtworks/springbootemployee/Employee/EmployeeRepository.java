8516a6642489618f9a212fe547964787edeb59eb
chanho <howard.chan@oocl.com>
2020-12-02 17:11:36 +0800

    [employee][Howard Chan]test: implement the test of should_return_all_employees_when_get_all_given_all_employees

--- src/main/java/com/thoughtworks/springbootemployee/Employee/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/Employee/EmployeeRepository.java
@@ -2,6 +2,14 @@ package com.thoughtworks.springbootemployee.Employee;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class EmployeeRepository {
+    private List<Employee> employees = new ArrayList<>();
+
+    public List<Employee> findAll() {
+        return null;
+    }
 }
--- src/main/java/com/thoughtworks/springbootemployee/Employee/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/Employee/EmployeeService.java
@@ -2,6 +2,19 @@ package com.thoughtworks.springbootemployee.Employee;
 
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @Service
 public class EmployeeService {
+    @Resource(name = "EmployeeRepository")
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> findAll() {
+        return null;
+    }
 }
