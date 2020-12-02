356abfbd41264fb14fbc7240f6eeb1d489b98073
Hins Chiu <chiuhi2@oocl.com>
2020-12-02 15:31:44 +0800

    [employee][HinsChiu]feat:add getOne API for EmployeeController

--- src/main/java/com/thoughtworks/springbootemployee/Employee/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/Employee/EmployeeController.java
@@ -2,7 +2,13 @@ package com.thoughtworks.springbootemployee.Employee;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +23,16 @@ public class EmployeeController {
         return ResponseEntity.ok(this.employees);
     }
 
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<Employee> getOne(@PathVariable Integer employeeId) {
+        Employee targetEmployee = this.employees.stream()
+                .filter(employee -> employee.getId().equals(employeeId))
+                .findFirst()
+                .orElse(null);
+
+        return targetEmployee == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(targetEmployee);
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Employee addOne(@RequestBody Employee employee) {
@@ -24,4 +40,6 @@ public class EmployeeController {
 
         return employee;
     }
+
+
 }
