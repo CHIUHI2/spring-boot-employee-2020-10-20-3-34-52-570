35e52936aeaf3dd1aaf2319b67f34fd97954db38
Hins Chiu <chiuhi2@oocl.com>
2020-12-02 15:13:15 +0800

    [employee][HinsChiu]feat:api for getAllEmployee

--- src/main/java/com/thoughtworks/springbootemployee/Employee/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/Employee/EmployeeController.java
@@ -2,14 +2,28 @@ package com.thoughtworks.springbootemployee.Employee;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+    List<Employee> employees = new ArrayList<>();
+
     @GetMapping
-    public ResponseEntity<Employee> getAll() {
-        return ResponseEntity.ok(null);
+    public ResponseEntity<List<Employee>> getAll() {
+        return ResponseEntity.ok(this.employees);
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> addOne(@RequestBody Employee employee) {
+        this.employees.add(employee);
+
+        return ResponseEntity.ok(employee);
     }
 }
