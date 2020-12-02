f182d40f07e010046dc3b68ab687973adec513dc
chanho <howard.chan@oocl.com>
2020-12-02 15:21:24 +0800

    [employee][Howard Chan]feat: add addOne API for EmployeeController

--- src/main/java/com/thoughtworks/springbootemployee/Employee/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/Employee/EmployeeController.java
@@ -1,11 +1,8 @@
 package com.thoughtworks.springbootemployee.Employee;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +18,10 @@ public class EmployeeController {
     }
 
     @PostMapping
-    public ResponseEntity<Employee> addOne(@RequestBody Employee employee) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee addOne(@RequestBody Employee employee) {
         this.employees.add(employee);
 
-        return ResponseEntity.ok(employee);
+        return employee;
     }
 }
