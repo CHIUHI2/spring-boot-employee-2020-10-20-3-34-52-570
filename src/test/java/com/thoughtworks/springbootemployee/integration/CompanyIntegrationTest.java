cb3523292af451b3e86c51dbc283221baf4acb54
Hins Chiu <chiuhi2@oocl.com>
2020-12-03 19:03:08 +0800

    [company][HinsChiu]test:implement test for should return last two Companies when CompanyController getAllWithPaginatiob given Companies 4, page 1, and page size 2

--- src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.integration;
 
 import com.thoughtworks.springbootemployee.dto.Company;
+import com.thoughtworks.springbootemployee.dto.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -41,5 +42,36 @@ public class CompanyIntegrationTest {
                 .andExpect(jsonPath("$[0].companyName").value("Company"))
                 .andExpect(jsonPath("$[0].employeesNumber").value(0))
                 .andExpect(jsonPath("$[0].employees").isEmpty());
+    }
+
+    @Test
+    void should_return_last_two_companies_when_get_all_with_pagination_given_companies_4_and_page_1_and_page_size_2() throws Exception {
+        //given
+        Company company1 = new Company("Company1");
+        this.companyRepository.save(company1);
+
+        Company company2 = new Company("Company2");
+        this.companyRepository.save(company2);
+
+        Company company3 = new Company("Company3");
+        this.companyRepository.save(company3);
+
+        Company company4 = new Company("Company4");
+        this.companyRepository.save(company4);
+
+        //when
+        //then
+        this.mockMvc.perform(get("/companies")
+                .param("page", "1")
+                .param("pageSize", "2")
+        ).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isString())
+                .andExpect(jsonPath("$[0].companyName").value("Company3"))
+                .andExpect(jsonPath("$[0].employeesNumber").value(0))
+                .andExpect(jsonPath("$[0].employees").isEmpty())
+                .andExpect(jsonPath("$[1].id").isString())
+                .andExpect(jsonPath("$[1].companyName").value("Company4"))
+                .andExpect(jsonPath("$[1].employeesNumber").value(0))
+                .andExpect(jsonPath("$[1].employees").isEmpty());
     }
 }
